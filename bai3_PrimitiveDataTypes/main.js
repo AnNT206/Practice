84ae0d4614c9be55ccdc9277936d8d0c148b0a83
AnNT206 <anngo16022006@gmail.com>
2025-12-31 14:18:06 +0700

    day2

---
+++ bai3_PrimitiveDataTypes/main.js
@@ -0,0 +1,28 @@
+alert("Welcome to JavaScript");
+
+//Kiểu number
+let soNguyen = 10;
+let soThuc = 8.8;
+
+//Kiểu boolean
+let check = true;
+
+//Kiểu undefined
+let diemToan;
+
+// Kiểu null
+let diemLy = null;
+
+//In kết quả
+console.log("Số nguyên:", soNguyen);
+console.log("Số thực:", soThuc);
+console.log("Kiểu boolean:", check);
+console.log("Giá trị undefined:", diemToan);
+console.log("Giá trị null:", diemLy);
+
+//In kiểu dữ liệu
+console.log("Kiểu dữ liệu của soNguyen:", typeof soNguyen);
+console.log("Kiểu dữ liệu của soThuc:", typeof soThuc);
+console.log("Kiểu dữ liệu của check:", typeof check);
+console.log("Kiểu dữ liệu của diemToan:", typeof diemToan);
+console.log("Kiểu dữ liệu của diemLy:", typeof diemLy);
