97fd85d2249f6f5a6bad0481c676c7f7278a805d
NgoAn <anngo16022006@gmail.com>
2026-01-02 16:12:33 +0700

    day4

---
+++ bai5_CacPhepToan/main.js
@@ -0,0 +1,15 @@
+// khởi tạo biến
+let a = 5;
+let b = 2;
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let chiaLayDu = a % b;
+// Xuất kết quả
+console.log(` Tổng của %s và %s là: %s`, a, b, tong);
+console.log(` Hiệu của %s và %s là: %s`, a, b, hieu);
+console.log(` Tích của %s và %s là: %s`, a, b, tich);
+console.log(` Thương của %s và %s là: %s`, a, b, thuong);
+console.log(` Chia lấy dư của %s và %s là: %s`, a, b, chiaLayDu);
