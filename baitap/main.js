e1fa1f8297dae710bd38a7bdf6b09b3c7aa2db4c
NgoAn <anngo16022006@gmail.com>
2026-01-17 23:48:32 +0700

    Add files via upload

---
+++ baitap/main.js
@@ -0,0 +1,7 @@
+// Khai báo biến
+let userName;
+
+// hiện thị hộp thoại nhập liệu
+userName = prompt("Nhập tên của bạn: ");
+console.log("Tên của bạn là: " + userName);
+console.log("Cố gắng lên " + userName + " sẽ làm được thôi");
